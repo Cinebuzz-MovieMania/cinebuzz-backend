0f6fd877027f536aaeb47210f35bd0c221859bb3
harsh5.agarwal <harsh5.agarwal@paytm.com>
2026-03-30 12:50:38 +0530

    init: project setup with Spring Boot + MySQL

---
+++ src/main/java/com/example/Cinebuzz/CinebuzzApplication.java
@@ -0,0 +1,13 @@
+package com.example.Cinebuzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinebuzzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinebuzzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Cinebuzz/CinebuzzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Cinebuzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinebuzzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
